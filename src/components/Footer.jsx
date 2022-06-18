5b90dd5ccbbf6ced9e434652338cd8f46781b47d
kaustubhreet <mishuyashu23gmail.com>
2022-06-18 20:33:28 +0530

    2nd change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,15 +9,15 @@ function Footer() {
             <h3>FoodStore</h3>
             <p>Foolish Developer is a blog website where you will find great tutorials on web design and development. Here each tutorial is beautifully described step by step with the required source code.</p>
             <ul className="socials">
-                <li><a href="#"><i className="fa fa-facebook"></i></a></li>
-                <li><a href="#"><i className="fa fa-instagram"></i></a></li>
+                <li><a href="https://www.facebook.com/kaustubh.reet"><i className="fa fa-facebook"></i></a></li>
+                <li><a href="https://www.instagram.com/delhi_foodies_dhaba/"><i className="fa fa-instagram"></i></a></li>
                 <li><a href="#"><i className="fa fa-google"></i></a></li>
                 <li><a href="#"><i className="fa fa-youtube"></i></a></li>
                 <li><a href="#"><i className="fa fa-linkedin"></i></a></li>
             </ul>
         </div>
         <div className="footer-bottom">
-            <p>copyright &copy;2022 <a href="#">FoodStore</a>  </p>
+            <p>copyright &copy;2022 <a href="/">FoodStore</a>  </p>
                     
         </div>
 
